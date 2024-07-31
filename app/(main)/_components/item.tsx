a9b271b8570e05dc13c8f393345c6e52b21b8cbe
HardiB-Salih <hardib.salih@gmail.com>
2024-07-31 15:54:49 +0300

    create a Untitled document and and chiled document to a parent.

--- app/(main)/_components/item.tsx
+++ app/(main)/_components/item.tsx
@@ -1,8 +1,13 @@
 "use client";
 
+import { Skeleton } from "@/components/ui/skeleton";
+import { api } from "@/convex/_generated/api";
 import { Id } from "@/convex/_generated/dataModel";
 import { cn } from "@/lib/utils";
-import { ChevronDown, ChevronRight, LucideIcon } from "lucide-react";
+import { useMutation } from "convex/react";
+import { ChevronDown, ChevronRight, LucideIcon, Plus } from "lucide-react";
+import { useRouter } from "next/navigation";
+import { toast } from "sonner";
 
 interface ItemProps {
   id?: Id<"documents">;
@@ -29,7 +34,34 @@ export default function Item({
   level = 0,
   onExpand,
 }: ItemProps) {
+  const create = useMutation(api.documents.create);
+  const router = useRouter();
   const ChevronIcon = expanded ? ChevronDown : ChevronRight;
+
+  const handleExpand = (
+    event: React.MouseEvent<HTMLDivElement, MouseEvent>,
+  ) => {
+    event.stopPropagation();
+    onExpand?.();
+  };
+
+  const onCreate = (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+    if (!id) return;
+    event.stopPropagation();
+    const promise = create({ title: "Untitled", parentDocument: id }).then(
+      (documentId) => {
+        if (!expanded) {
+          onExpand?.();
+        }
+        // router.push(`/documents/${documentId}`);
+      },
+    );
+    toast.promise(promise, {
+      loading: "Creating a new note...",
+      success: "New note created",
+      error: "Faild to create a new note",
+    });
+  };
 
   return (
     <div
@@ -39,31 +71,57 @@ export default function Item({
         paddingLeft: level ? `${level * 12 + 12}px` : "12px",
       }}
       className={cn(
-        "group min-h-[27px] text-sm py-1 pr-3 w-full hover:bg-primary/5 flex items-center text-muted-foreground font-medium",
+        "group flex min-h-[27px] w-full items-center py-1 pr-3 text-sm font-medium text-muted-foreground hover:bg-primary/5",
         active && "bg-primary/5 text-primary",
       )}
     >
       {!!id && (
         <div
           role="button"
-          className="h-full rounded-sm hover:bg-neutral-300 dark:bg-neutral-600 mr-1"
-          onClick={() => {}}
+          className="mr-1 h-full rounded-sm hover:bg-neutral-300 dark:bg-neutral-600"
+          onClick={handleExpand}
         >
           <ChevronIcon className="size-4 shrink-0 text-muted-foreground/50" />
         </div>
       )}
 
       {documentIcon ? (
-        <div className="shrink-0 mr-2 text-[18px]">{documentIcon}</div>
+        <div className="mr-2 shrink-0 text-[18px]">{documentIcon}</div>
       ) : (
-        <Icon className="shrink-0 h-[18px] mr-2 text-muted-foreground" />
+        <Icon className="mr-2 h-[18px] shrink-0 text-muted-foreground" />
       )}
       <span className="truncate">{label}</span>
       {isSearch && (
-        <kbd className="text-[10px] pointer-events-none inline-flex h-5 select-none items-center gap-1 rounded border bg-muted px-1.5 font-medium text-muted-foreground opacity-100 ml-auto font-mono">
+        <kbd className="pointer-events-none ml-auto inline-flex h-5 select-none items-center gap-1 rounded border bg-muted px-1.5 font-mono text-[10px] font-medium text-muted-foreground opacity-100">
           <span className="text-xs">&#8984;</span>K
         </kbd>
+      )}
+
+      {!!id && (
+        <div className="ml-auto flex items-center gap-x-2">
+          <div
+            role="button"
+            onClick={onCreate}
+            className="ml-auto h-full rounded-sm opacity-0 hover:bg-neutral-300 group-hover:opacity-100 dark:hover:bg-neutral-600"
+          >
+            <Plus className="size-4 text-muted-foreground" />
+          </div>
+        </div>
       )}
     </div>
   );
 }
+
+Item.Skeleton = function ItemSkeleton({ level }: { level?: number }) {
+  return (
+    <div
+      style={{
+        paddingLeft: level ? `${level * 12 + 25}px` : "12px",
+      }}
+      className="flex gap-x-2 py-[3px]"
+    >
+      <Skeleton className="h-4 w-4" />
+      <Skeleton className="h-4 w-[30%]" />
+    </div>
+  );
+};
--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -13,17 +13,17 @@ import { toast } from "sonner";
 import { usePathname } from "next/navigation";
 import { useMediaQuery } from "usehooks-ts";
 
-import { useMutation, useQuery } from "convex/react";
+import { useMutation } from "convex/react";
 import { api } from "@/convex/_generated/api";
 
 import { cn } from "@/lib/utils";
 
 import UserItem from "./UserItem";
 import Item from "./item";
+import DocumentList from "./document_list";
 
 export default function Navigation() {
   const create = useMutation(api.documents.create);
-  const documents = useQuery(api.documents.get);
 
   const pathname = usePathname();
   const isMobile = useMediaQuery("(max-width: 768px)");
@@ -146,7 +146,7 @@ export default function Navigation() {
           <Item onClick={handleCreate} label="New page" icon={PlusCircle} />
         </div>
         <div className="mt-4">
-          {documents?.map((doc) => <p key={doc._id}>{doc.title}</p>)}
+          <DocumentList />
         </div>
         <div
           onMouseDown={handleMouseDown}
